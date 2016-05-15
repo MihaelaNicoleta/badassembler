3ee62fa0e89f54c566eac6ef5d61bf73cd8ce9b9
MihaelaNicoleta <raumiha21@gmail.com>
2016-05-16 01:10:07 +0300

    Add function to decode DBUS

--- Assembler/Sequencer.cs
+++ Assembler/Sequencer.cs
@@ -8,7 +8,6 @@ namespace Assembler
 {
     class Sequencer
     {
-
         public int step = 0;
 
         public void runStepSimulation()
@@ -42,36 +41,37 @@ namespace Assembler
 
         public void runSimulation()
         {
-            GraphicChanger gc = new GraphicChanger();
 
-            gc.Pd0s();
+            GraphicChanger graphicChanger = new GraphicChanger();
+            graphicChanger.Pd0s();
         }
 
         private void decodeSBUS(UInt64 MIRCode)
         {
+            GraphicChanger graphicChanger = new GraphicChanger();
             UInt16 sbus = (UInt16)((MIRCode & 515396075520) >> 35);
             switch (sbus)
             {
-                case 0x1: //PdIR[Off]s
-                    //PdIROffs();
+                case 0x1: //PdIRs
+                    graphicChanger.PdIRs();
                     //SBUS = (UInt16)(IR & 0x00FF);
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
 
                 case 0x2: //PdFLAGs		
-                    //PdFLAGs();
+                    graphicChanger.PdFLAGs();
                     //SBUS = (UInt16)(FLAG);
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
 
                 case 0x3: //PdSPs
-                    //PdSPs();
+                    graphicChanger.PdSPs();
                     //SBUS = (UInt16)(SP);
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
 
                 case 0x4: //PdTs
-                    //PdTs();
+                    graphicChanger.PdTs();
                     //SBUS = (UInt16)(T);
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
@@ -84,25 +84,25 @@ namespace Assembler
                     break;
 
                 case 0x6: //PdPCs	
-                    //PdPCs();
+                    graphicChanger.PdPCs();
                     //SBUS = (UInt16)(PC);
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
 
                 case 0x7: //PdIVRs	
-                    //PdIVRs();
+                    graphicChanger.PdIVRs();
                     //SBUS = (UInt16)(IVR);
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
 
                 case 0x8: //PdADRs		
-                    //PdADRs();
+                    graphicChanger.PdADRs();
                     //SBUS = (UInt16)(ADR);
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
 
                 case 0x9: //PdMDRs	
-                    //PdMDRs();
+                    graphicChanger.PdMDRs();
                     //SBUS = (UInt16)(MDR);
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
@@ -114,70 +114,74 @@ namespace Assembler
                     switch (nr_reg)
                     {
                         case 0x1:
-                            //PdRg1();
+                            graphicChanger.PdR1();
                             break;
                         case 0x2:
-                            //PdRg2();
+                            graphicChanger.PdR2();
                             break;
                         case 0x3:
-                            //PdRg3();
+                            graphicChanger.PdR3();
                             break;
                         case 0x4:
-                            // PdRg4();
+                            graphicChanger.PdR4();
                             break;
                         case 0x5:
-                            //PdRg5();
+                            graphicChanger.PdR5();
                             break;
                         case 0x6:
-                            //PdRg6();
+                            graphicChanger.PdR6();
                             break;
                         case 0x7:
-                            //PdRg7();
+                            graphicChanger.PdR7();
                             break;
                         case 0x8:
-                            //PdRg8();
+                            graphicChanger.PdR8();
                             break;
                         case 0x9:
-                            //PdRg9();
+                            graphicChanger.PdR9();
                             break;
                         case 0xA:
-                            //PdRg10();
+                            graphicChanger.PdR10();
                             break;
                         case 0xB:
-                            //PdRg11();
+                            graphicChanger.PdR11();
                             break;
                         case 0xC:
-                            //PdRg12();
+                            graphicChanger.PdR12();
                             break;
                         case 0xD:
-                            //PdRg13();
+                            graphicChanger.PdR13();
                             break;
                         case 0xE:
-                            // PdRg14();
+                            graphicChanger.PdR14();
                             break;
                         case 0xF:
-                            // PdRg15();
+                            graphicChanger.PdR15();
                             break;
                         default:
-                            //PdRg0();
+                            graphicChanger.PdR0();
                             break;
                     }
+
+                    graphicChanger.PdRGs();
+                    //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
+
                 case 0xB: //Pd0s
-                    //Pd0s();
+                    graphicChanger.Pd0s();
                     //SBUS = 0;
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
 
                 case 0xC: //Pd-1s
-                    //Pdm1s();
+                    graphicChanger.Pdminus1s();
                     //Int16 v = -1;
                     //SBUS = (UInt16)v;
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
 
                 case 0xD: //Pd1s
-                    //Pd1s();
+                    graphicChanger.Pd1s();
                     //SBUS = (UInt16)(1);
                     //SBUSlabel.Text = Convert_Binary(SBUS.ToString(), 16);
                     break;
@@ -187,9 +191,136 @@ namespace Assembler
             }
         }
 
-        private void decodeDBUS()
+        private void decodeDBUS(UInt64 MIRCode)
         {
+            GraphicChanger graphicChanger = new GraphicChanger();
+            UInt16 dbus = (UInt16)((MIRCode & 32212254720) >> 31);
+            switch (dbus)
+            {
+                case 0x1: //PdIR[Off]d
+                    graphicChanger.PdIRd();
+                    //DBUS = (UInt16)(IR & 0x00FF);
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
 
+                case 0x2: //PdFLAGd
+                    graphicChanger.PdFLAGd();
+                    //DBUS = (UInt16)(FLAG);
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
+
+                case 0x3: //PdSPd
+                    graphicChanger.PdSPd();
+                    //DBUS = (UInt16)(SP);
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
+
+                case 0x4: //PdTd
+                    graphicChanger.PdTd();
+                    //DBUS = (UInt16)(T);
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
+
+                case 0x5: //PdnTd
+                    //PdnTd();
+                    //UInt16 t = T;
+                    //DBUS = (UInt16)(~t);
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
+
+                case 0x6: //PdPCd
+                    graphicChanger.PdPCd();
+                    //DBUS = (UInt16)(PC);
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
+
+                case 0x7: //PdIVRd
+                    graphicChanger.PdIVRd();
+                    //DBUS = (UInt16)(IVR);
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
+
+                case 0x8: //PdADRd
+                    graphicChanger.PdADRd();
+                    //DBUS = (UInt16)(ADR);
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
+
+                case 0x9: //PdMDRd
+                    graphicChanger.PdMDRd();
+                    //DBUS = (UInt16)(MDR);
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
+
+                case 0xA: //PdRGd
+                    //int nr_reg = IR & 0x000F; //preiau reg din camp RS din IR
+                    //DBUS = Convert.ToUInt16(R[nr_reg]);
+
+                    var  nr_reg = 1;
+                    switch (nr_reg)
+                    {
+                        case 0x1:
+                            graphicChanger.PdR1();
+                            break;
+                        case 0x2:
+                            graphicChanger.PdR2();
+                            break;
+                        case 0x3:
+                            graphicChanger.PdR3();
+                            break;
+                        case 0x4:
+                            graphicChanger.PdR4();
+                            break;
+                        case 0x5:
+                            graphicChanger.PdR5();
+                            break;
+                        case 0x6:
+                            graphicChanger.PdR6();
+                            break;
+                        case 0x7:
+                            graphicChanger.PdR7();
+                            break;
+                        case 0x8:
+                            graphicChanger.PdR8();
+                            break;
+                        case 0x9:
+                            graphicChanger.PdR9();
+                            break;
+                        case 0xA:
+                            graphicChanger.PdR10();
+                            break;
+                        case 0xB:
+                            graphicChanger.PdR11();
+                            break;
+                        case 0xC:
+                            graphicChanger.PdR12();
+                            break;
+                        case 0xD:
+                            graphicChanger.PdR13();
+                            break;
+                        case 0xE:
+                            graphicChanger.PdR14();
+                            break;
+                        case 0xF:
+                            graphicChanger.PdR15();
+                            break;
+                        default:
+                            graphicChanger.PdR0();
+                            break;
+                    }
+                    graphicChanger.PdRGd();
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
+
+                case 0xB: //Pd0d
+                    graphicChanger.Pd0d();
+                    //DBUS = 0;
+                    //DBUSlabel.Text = Convert_Binary(DBUS.ToString(), 16);
+                    break;
+
+                default: //none
+                    break;
+            }
         }
 
         private void decodeRBUS()
